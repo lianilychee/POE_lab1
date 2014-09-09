7935ac7b2775d17f8f1c56bdb6e7cc54bd6f762f
lianilychee <lye.liani@gmail.com>
2014-09-09 12:20:17 -0400

    Potentiometer to modify delay added

--- lab1_part2/lab1_part2.ino
+++ lab1_part2/lab1_part2.ino
@@ -2,13 +2,16 @@ const int button = 8;
 const int led1 = 11;
 const int led2 = 12;
 const int led3 = 13;
+const float pot = A2;
 
+float potVal = 0;
 int pushCount = 0;
 int buttonCurrent = 0;
 int buttonPrevious = 0;
 
 void setup () {
   pinMode(button, INPUT);
+  pinMode(pot, INPUT);
   pinMode(led2, OUTPUT);
   pinMode(led1, OUTPUT);
   pinMode(led3, OUTPUT);
@@ -16,7 +19,10 @@ void setup () {
 }
 
 void loop () {
+
   buttonCurrent = digitalRead(button);
+  potVal = analogRead(pot) * 2;
+  Serial.println(potVal);
 
   if (buttonCurrent != buttonPrevious) {
     if (buttonCurrent == HIGH) {
@@ -36,42 +42,42 @@ void loop () {
   buttonPrevious = buttonCurrent;
 
   if (pushCount == 1) {
-    Serial.println("all on");
+    //Serial.println("all on");
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
     digitalWrite(led3, HIGH);
   } 
   else if (pushCount == 2) {
-    Serial.println("all off");
+    //Serial.println("all off");
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
     digitalWrite(led1, LOW);
   }
   else if (pushCount == 3) {
-    Serial.println("flashing");
+    //Serial.println("flashing");
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
     digitalWrite(led3, HIGH);
-    delay(400);
+    delay(potVal);
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
     digitalWrite(led1, LOW);
-    delay(400);
+    delay(potVal);
   }
   else if (pushCount == 4) {
-    Serial.println("bouncing");
+    //Serial.println("bouncing");
     digitalWrite(led1, HIGH);
-    delay(400);
+    delay(potVal);
     digitalWrite(led1, LOW);
-    delay(400);
+    delay(potVal);
     digitalWrite(led2, HIGH);
-    delay(400);
+    delay(pot);
     digitalWrite(led2, LOW);
-    delay(400);
+    delay(potVal);
     digitalWrite(led3, HIGH);
-    delay(400);
+    delay(potVal);
     digitalWrite(led3, LOW);
-    delay(400);
+    delay(potVal);
   }
   
 }
